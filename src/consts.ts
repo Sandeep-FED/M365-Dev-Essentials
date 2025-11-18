c113d99b30a1544c18e9c39dfd6a2c2a3a87cd09
Sandeep-FED <sandeep.ps0124@outlook.com>
2025-11-18 21:19:59 +0530

    feat: add UI components for badge, breadcrumb, button, infinite scroll, logo, pagination, and separator
    - Introduced Badge component for displaying status or notifications.
    - Created Breadcrumb component for navigation hierarchy.
    - Added Button component with various styles and sizes.
    - Implemented Infinite Scroll component for continuous content loading.
    - Developed Logo component for branding.
    - Built Pagination component for navigating through pages of content.
    - Added Separator component for visual separation of content.
    
    refactor: update global constants and styles
    
    - Updated site constants with new site information and social links.
    - Refactored global styles to use new font and typography settings.
    - Introduced new typography styles for better content presentation.
    
    fix: update blog and index pages to use new Navbar component
    
    - Replaced Header component with Navbar in BlogPost and Index pages for consistent navigation.
    - Adjusted HTML structure and styles for improved layout and accessibility.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,118 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+import type { IconMap, SocialLink, Site } from "@/types"
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE: Site = {
+  title: "M365 Dev Essentials",
+  description:
+    "Senior software engineer | Microsoft MVP | Microsoft 365 Developer | Sharing knowledge and insights on Microsoft 365 development.",
+  href: "https://m365-dev-essentials.vercel.app",
+  author: "Sandeep P S",
+  locale: "en-US",
+  location: "India",
+  email: "sandeep.ps0124@outlook.com",
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: "/",
+    label: "home",
+  },
+  {
+    href: "/projects",
+    label: "projects",
+  },
+  {
+    href: "/blog",
+    label: "blog",
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: "https://github.com/cojocaru-david?ref=personal-website",
+    label: "GitHub",
+  },
+  {
+    href: "mailto:contact@cojocarudavid.me",
+    label: "Email",
+  },
+  {
+    href: "tel:+40764132266",
+    label: "Phone",
+  },
+  {
+    href: "https://www.instagram.com/david._.cojo?ref=personal-website",
+    label: "Instagram",
+  },
+  {
+    href: "/rss.xml",
+    label: "RSS",
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: "lucide:globe",
+  GitHub: "lucide:github",
+  Instagram: "lucide:instagram",
+  Phone: "lucide:phone",
+  Email: "lucide:mail",
+  RSS: "lucide:rss",
+}
+
+export interface Category {
+  text: string
+  logo: string
+}
+
+export type Technologies = {
+  "Web Development": Category[]
+  "Development Tools": Category[]
+  "Hosting and Cloud Services": Category[]
+  "Operating Systems": Category[]
+  "Other Programming Languages and Technologies": Category[]
+  "Web Servers": Category[]
+  Databases: Category[]
+  "Other Software": Category[]
+}
+
+export const technologies: Technologies = {
+  "Web Development": [
+    { text: "HTML", logo: "mdi:language-html5" },
+    { text: "JavaScript", logo: "mdi:language-javascript" },
+    { text: "CSS", logo: "mdi:language-css3" },
+    { text: "PHP", logo: "mdi:language-php" },
+    { text: "Astro", logo: "simple-icons:astro" },
+    { text: "Tailwind CSS", logo: "mdi:tailwind" },
+  ],
+  "Development Tools": [
+    { text: "Visual Studio Code", logo: "mdi:visual-studio-code" },
+    { text: "Git", logo: "mdi:git" },
+  ],
+  "Hosting and Cloud Services": [
+    { text: "DigitalOcean", logo: "mdi:digital-ocean" },
+    { text: "Cloudflare", logo: "cib:cloudflare" },
+    { text: "Netlify", logo: "cib:netlify" },
+  ],
+  "Operating Systems": [
+    { text: "Windows", logo: "mdi:windows" },
+    { text: "Ubuntu", logo: "mdi:ubuntu" },
+  ],
+  "Other Programming Languages and Technologies": [
+    { text: "Lua", logo: "mdi:language-lua" },
+    { text: "Golang", logo: "mdi:language-go" },
+    { text: "Node.js", logo: "mdi:nodejs" },
+  ],
+  "Web Servers": [
+    { text: "Apache", logo: "cib:apache" },
+    { text: "Nginx", logo: "cib:nginx" },
+  ],
+  Databases: [
+    { text: "MySQL", logo: "cib:mysql" },
+    { text: "MongoDB", logo: "cib:mongodb" },
+  ],
+  "Other Software": [
+    { text: "Discord", logo: "mdi:discord" },
+    { text: "Spotify", logo: "mdi:spotify" },
+    { text: "Visual Studio", logo: "mdi:visual-studio" },
+    { text: "Brave", logo: "cib:brave" },
+  ],
+}
